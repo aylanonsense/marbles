c09caec422c9dfc4383209ca3b54c4e85065006a
bridgs <bridgs.dev@gmail.com>
2020-05-08 16:08:03 -0400

    Adding warning when overwriting a saved game

--- source/game/TitleScreenScene.lua
+++ source/game/TitleScreenScene.lua
@@ -12,6 +12,8 @@ function TitleScreenScene:init(canContinue)
   self.backgroundImage = imageCache.loadImage("images/title/title-bg.png")
   self.selectionBackgroundImage = imageCache.loadImage("images/title/selection-bg.png")
   self.selectionBackgroundImageWidth, self.selectionBackgroundImageHeight = self.selectionBackgroundImage:getSize()
+  self.warningImage = imageCache.loadImage("images/title/warning.png")
+  self.warningImageWidth, self.warningImageHeight = self.warningImage:getSize()
   self.cursorImage = imageCache.loadImage("images/title/cursor.png")
   self.cursorImageWidth, self.cursorImageHeight = self.cursorImage:getSize()
   self.cursorBlinkFrames = 0
@@ -26,12 +28,16 @@ function TitleScreenScene:init(canContinue)
       "NEW GAME"
     }
   end
+  self.canContinue = canContinue
   self.hasChosenOption = false
+  self.isDisplayingWarning = false
+  self.framesUntilActive = 50
   sceneTransition:transitionIn()
 end
 
 function TitleScreenScene:update()
-  self.cursorBlinkFrames = (self.cursorBlinkFrames + 1) % (self.hasChosenOption and 10 or 48)
+  self.framesUntilActive = math.max(0, self.framesUntilActive - 1)
+  self.cursorBlinkFrames = (self.cursorBlinkFrames + 1) % (self.hasChosenOption and 6 or 48)
   sceneTransition:update()
 end
 
@@ -42,19 +48,22 @@ function TitleScreenScene:draw()
   local y = 200 - 10 * #self.options
   for i, option in ipairs(self.options) do
     local textWidth, textHeight = playdate.graphics.getTextSize(option)
-    playdate.graphics.drawText(option, 160, y)
+    playdate.graphics.drawText(option, 150, y)
     if i == self.selectionIndex then
-      if self.cursorBlinkFrames < (self.hasChosenOption and 5 or 34) then
-        self.cursorImage:draw(132, y - 1)
+      if self.cursorBlinkFrames < (self.hasChosenOption and 3 or 34) then
+        self.cursorImage:draw(124, y - 1)
       end
     end
     y += textHeight
+  end
+  if self.isDisplayingWarning then
+    self.warningImage:draw(200 - self.warningImageWidth / 2, 120 - self.warningImageHeight / 2)
   end
   sceneTransition:draw()
 end
 
 function TitleScreenScene:upButtonDown()
-  if not self.hasChosenOption then
+  if not self.hasChosenOption and not self.isDisplayingWarning then
     self.selectionIndex -= 1
     if self.selectionIndex < 1 then
       self.selectionIndex = #self.options
@@ -64,7 +73,7 @@ function TitleScreenScene:upButtonDown()
 end
 
 function TitleScreenScene:downButtonDown()
-  if not self.hasChosenOption then
+  if not self.hasChosenOption and not self.isDisplayingWarning then
     self.selectionIndex += 1
     if self.selectionIndex > #self.options then
       self.selectionIndex = 1
@@ -74,11 +83,20 @@ function TitleScreenScene:downButtonDown()
 end
 
 function TitleScreenScene:AButtonDown()
-  if not self.hasChosenOption then
-    self.hasChosenOption = true
-    self.cursorBlinkFrames = 0
-    sceneTransition:transitionOut(function()
-      self:endScene(self.options[self.selectionIndex])
-    end)
+  if not self.hasChosenOption and self.framesUntilActive <= 0 then
+    if self.canContinue and self.options[self.selectionIndex] == "NEW GAME" and not self.isDisplayingWarning then
+      self.isDisplayingWarning = true
+    else
+      self.hasChosenOption = true
+      self.cursorBlinkFrames = 0
+      self.isDisplayingWarning = false
+      sceneTransition:transitionOut(function()
+        self:endScene(self.options[self.selectionIndex])
+      end)
+    end
   end
+end
+
+function TitleScreenScene:BButtonDown()
+  self.isDisplayingWarning = false
 end
