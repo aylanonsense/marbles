090eb980d6a0648aaf749437ea2598f17392ba97
bridgs <bridgs.dev@gmail.com>
2021-03-31 19:24:19 -0400

    Remove SKIP_LOGOstat

--- source/config.lua
+++ source/config.lua
@@ -5,7 +5,6 @@ config = {
   DEBUG_MODE_ENABLED = allowDebugMode,
   MUSIC_VOLUME = 0.7,
   SOUND_VOLUME = 1.0,
-  SKIP_LOGO = true,
   BACKWARDS_COMPATIBILITY_MODE = false,
   LAUNCH_LEVEL_EDITOR = false
 }
